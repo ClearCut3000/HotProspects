5d75cee4f98f9c5322c47760b27ca3f98774754a
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-10-30 21:50:09 +0300

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Николай Никитин on 30.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
